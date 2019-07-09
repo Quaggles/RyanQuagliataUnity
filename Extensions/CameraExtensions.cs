1695f03c7970a755a9b0be78bc9220410a70f502
Quaggles <r.quagliata@qut.edu.au>
2019-07-09 14:34:16 +1000

    Added SetClippingPlanes Camera extension method

---
+++ Extensions/CameraExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace RyanQuagliataUnity.Extensions {
+	public static class CameraExtensions {
+		public static void SetClippingPlanes(this Camera that, Vector2 clippingPlanes) {
+			that.nearClipPlane = clippingPlanes.x;
+			that.farClipPlane = clippingPlanes.y;
+		}
+	}
+}
