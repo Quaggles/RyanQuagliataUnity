ef29c3f188cb54cd5e3346764f33c9d50efa8f4a
Quaggles <r.quagliata@qut.edu.au>
2019-07-15 13:16:13 +1000

    Added Mesh Serializer

---
+++ Scripts/Editor/MeshSerializer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using RyanQuagliata.Extensions;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace RyanQuagliataUnity.Scripts.Editor {
+	public static class MeshSerializer {
+		[InitializeOnLoadMethod]
+		static void Listen() => PrefabUtility.prefabInstanceUpdated += PrefabInstanceUpdate;
+
+		static void PrefabInstanceUpdate(GameObject instance) {
+			var prefab = PrefabUtility.GetCorrespondingObjectFromSource(instance);
+			string prefabPath = AssetDatabase.GetAssetPath(prefab);
+
+			// Create a tuple of the mesh filters on the instance we're saving and the new prefab
+			var filters = instance.GetComponentsInChildren<MeshFilter>().Zip(
+				prefab.GetComponentsInChildren<MeshFilter>(),
+				(instanceFilter, prefabFilter) => (instanceFilter, prefabFilter));
+
+			// Find all meshes that aren't already assets
+			var nonAssets = filters.Where(x => !IsAsset(x.instanceFilter.sharedMesh));
+			if (nonAssets.IsEmpty()) return; // If there are no non-asset mesh's no work to be done
+
+			// Confirm to the user they want save
+			if (!EditorUtility.DisplayDialog("Warning",
+				$"The GameObject you are creating a prefab from contains {nonAssets.Count()} dynamic meshes, their data will be lost unless they are saved as assets, do you want to save them?\n{string.Join(",\n", nonAssets.Select(x => x.instanceFilter.name))}",
+				"Yes", "No")) {
+				return;
+			}
+
+
+			bool valid = false;
+			string path = "";
+
+			if (nonAssets.Count() == 1) { // Individual file export
+				var meshFilter = nonAssets.First();
+
+				// Ensure the user chooses a valid path
+				while (!valid) {
+					path = EditorUtility.SaveFilePanel("Export Mesh", Path.GetDirectoryName(prefabPath),
+						meshFilter.instanceFilter.name, "asset");
+					if (string.IsNullOrWhiteSpace(path)) return;
+
+					// If valid then make path relative, otherwise try again
+					if (path.StartsWith(Application.dataPath)) {
+						valid = true;
+						path = "Assets" + path.Substring(Application.dataPath.Length);
+					} else {
+						EditorUtility.DisplayDialog("Error",
+							$"File must reside within {Application.dataPath}", "OK");
+					}
+				}
+
+				// Create the new mesh asset and link it up to the MeshFilter in the new prefab
+				try {
+					AssetDatabase.CreateAsset(meshFilter.instanceFilter.sharedMesh, path);
+					meshFilter.prefabFilter.sharedMesh = AssetDatabase.LoadAssetAtPath<Mesh>(path);
+				} catch (Exception ex) {
+					EditorUtility.DisplayDialog("Error", ex.ToString(), "OK");
+				}
+			} else { // Bulk folder export
+				// Ensure the user chooses a valid path
+				while (!valid) {
+					path = EditorUtility.SaveFolderPanel("Export Meshes", Path.GetDirectoryName(prefabPath),
+						Path.GetDirectoryName(prefabPath));
+					if (string.IsNullOrWhiteSpace(path)) return;
+
+					// If valid then make path relative, otherwise try again
+					if (path.StartsWith(Application.dataPath)) {
+						valid = true;
+						path = "Assets" + path.Substring(Application.dataPath.Length);
+					} else {
+						EditorUtility.DisplayDialog("Error",
+							$"Selected folder must reside within {Application.dataPath}", "OK");
+					}
+				}
+
+				// Create the new mesh assets and link it up to the MeshFilter in the new prefab
+				int progress = 0;
+				int total = nonAssets.Count();
+				foreach (var meshFilter in nonAssets) {
+					try {
+						var validPath =
+							AssetDatabase.GenerateUniqueAssetPath(Path.Combine(path,
+								$"{meshFilter.instanceFilter.name}.asset"));
+						EditorUtility.DisplayProgressBar("Serializing Meshes", $"Saving {validPath}",
+							progress / (float) total);
+						AssetDatabase.CreateAsset(meshFilter.instanceFilter.sharedMesh, validPath);
+						meshFilter.prefabFilter.sharedMesh = AssetDatabase.LoadAssetAtPath<Mesh>(validPath);
+						progress++;
+					} catch (Exception ex) {
+						EditorUtility.DisplayDialog("Error", ex.ToString(), "OK");
+					}
+				}
+
+				EditorUtility.ClearProgressBar();
+			}
+
+			AssetDatabase.SaveAssets();
+		}
+
+		static bool IsAsset(Object obj) => !string.IsNullOrWhiteSpace(AssetDatabase.GetAssetPath(obj));
+	}
+}
