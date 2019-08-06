4e97c8f295e06021dfaeef14c1dc1b1ddfdea9d5
Quaggles <r.quagliata@qut.edu.au>
2019-08-06 16:16:48 +1000

    Added LayerMaskExtensions

---
+++ Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace RyanQuagliataUnity.Extensions {
+	public static class LayerMaskExtensions {
+		/// <summary>
+		/// Finds if the LayerMask contains a layer
+		/// </summary>
+		/// <param name="that"></param>
+		/// <param name="layer"></param>
+		/// <returns></returns>
+		public static bool ContainsLayer(this LayerMask that, int layer) => that == (that | (1 << layer));
+	}
+}
