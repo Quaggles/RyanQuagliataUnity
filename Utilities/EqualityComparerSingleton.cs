d8e7e7c963bacbda3ece7537b0db47f9b8182beb
Ryan Quagliata <r.quagliata@qut.edu.au>
2019-10-24 11:36:03 +1000

    Added EqualityComparerSingleton

---
+++ Utilities/EqualityComparerSingleton.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RyanQuagliataUnity.Utilities {
+	public abstract class EqualityComparerSingleton<T1, T2> : IEqualityComparer<T2> where T1 : new() {
+		public static EqualityComparerSingleton<T1, T2> Instance => instance ??= new T1();
+		private static EqualityComparerSingleton<T1, T2> instance;
+		public abstract bool Equals(T2 x, T2 y);
+		public abstract int GetHashCode(T2 obj);
+	}
+}
