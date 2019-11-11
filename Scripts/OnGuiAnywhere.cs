59729f2e420c7ef18080bab5d0a025d975ca0d67
Ryan Quagliata <r.quagliata@qut.edu.au>
2019-11-11 15:21:28 +1000

    Added OnGuiAnywhere

---
+++ Scripts/OnGuiAnywhere.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace RyanQuagliataUnity {
+    // ReSharper disable once InconsistentNaming
+    public class OnGuiMonoBehaviour : MonoBehaviour {
+        // ReSharper disable once InconsistentNaming
+        public Queue<Action> OnGuiActions = new Queue<Action>();
+#if UNITY_EDITOR
+        private void OnDrawGizmos() {
+            while (OnGuiActions.Count > 0) {
+                var i = OnGuiActions.Dequeue();
+                i.Invoke();
+            }
+        }
+#endif
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public static class OnGuiAnywhere {
+        private static OnGuiMonoBehaviour onGuiHost;
+
+#if UNITY_EDITOR
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void Initialize() {
+            GetHandlesHost();
+        }
+#endif
+
+        static OnGuiMonoBehaviour GetHandlesHost() {
+            if (!onGuiHost) {
+                onGuiHost = new GameObject().AddComponent<OnGuiMonoBehaviour>();
+                onGuiHost.gameObject.name = $"[{nameof(onGuiHost)}]";
+                Object.DontDestroyOnLoad(onGuiHost);
+            }
+
+            return onGuiHost;
+        }
+
+        public static void AddGeneric(Action action) => GetHandlesHost().OnGuiActions.Enqueue(action);
+
+        public static class Handles {
+            public static void Label(Vector3 position, string label) {
+#if UNITY_EDITOR
+                void Action() => UnityEditor.Handles.Label(position, label);
+                AddGeneric(Action);
+#endif
+            }
+        }
+
+        public static class Gizmos {
+            public static void DrawLine(Vector3 from, Vector3 to) {
+#if UNITY_EDITOR
+                void Action() => UnityEngine.Gizmos.DrawLine(from, to);
+                AddGeneric(Action);
+#endif
+            }
+        }
+    }
+}
