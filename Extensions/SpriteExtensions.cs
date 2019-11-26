0de0e2e5aa0e0c6ae9c9b39e0663b4877369e74b
Ryan Quagliata <r.quagliata@qut.edu.au>
2019-11-26 11:53:18 +1000

    Added some more sprite extension colours

--- Extensions/SpriteExtensions.cs
+++ Extensions/SpriteExtensions.cs
@@ -4,7 +4,14 @@ namespace RyanQuagliataUnity.Extensions {
 	public static class SpriteExtensions {
 		private static Sprite clearSprite;
 		public static Sprite ClearSprite => clearSprite != null ? clearSprite : clearSprite = Create(Texture2DExtensions.CreateSolidTexture2D(1, 1, new Color32(1, 1, 1, 0)));
+		
+		private static Sprite whiteSprite;
+		public static Sprite WhiteSprite => whiteSprite != null ? whiteSprite : whiteSprite = Create(Texture2DExtensions.CreateSolidTexture2D(1, 1, Color.white));
 
+		
+		private static Sprite blackSprite;
+		public static Sprite BlackSprite => blackSprite != null ? blackSprite : blackSprite = Create(Texture2DExtensions.CreateSolidTexture2D(1, 1, Color.black));
+		
 		/// <summary>
 		/// Sprite.Create but with less pointless arguments
 		/// </summary>
