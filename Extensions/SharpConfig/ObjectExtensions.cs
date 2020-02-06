48a3a678f9e6bf81c44b46a7129604e52f9a3a72
Ryan Quagliata <r.quagliata@qut.edu.au>
2020-02-06 14:48:01 +1000

    Removed sharpconfig extensions

--- Extensions/SharpConfig/ObjectExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace RyanQuagliataUnity.Extensions.SharpConfig {
-	public static class ObjectExtensions {
-		public static void InstantiateAndConfigure(this UnityEngine.Object that) {
-			Object.Instantiate(that);
-		}
-	}
-}
