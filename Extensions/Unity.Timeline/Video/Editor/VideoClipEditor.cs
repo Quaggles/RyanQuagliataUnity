f695ba75fb67ecbe4d632be6d2272316391ff8d7
Ryan Quagliata <r.quagliata@qut.edu.au>
2020-09-11 10:38:42 +1000

    VideoPlayer clips will now have their clip names set to the name of the video file

---
+++ Extensions/Unity.Timeline/Video/Editor/VideoClipEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor.Timeline;
+using UnityEngine.Timeline;
+
+namespace RyanQuagliataUnity.Extensions.Unity.Timeline.Video.Editor {
+	[CustomTimelineEditor(typeof(VideoScriptPlayableAsset))]
+	public class VideoClipEditor : ClipEditor {
+		public override void OnClipChanged(TimelineClip clip) {
+			if (clip.asset is VideoScriptPlayableAsset clipAsset) {
+				if (clipAsset.videoClip) {
+					clip.displayName = clipAsset.videoClip.name;
+				} else {
+					clip.displayName = "Empty";
+				}
+			}
+		}
+	}
+}
