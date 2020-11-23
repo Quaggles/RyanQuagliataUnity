dedb03b7683e9eac2b98752449f00fb7a413b242
Ryan Quagliata <r.quagliata@qut.edu.au>
2020-11-23 11:46:21 +1000

    Fixed error with lighting asset import

--- Scripts/Editor/ModelPostprocessor.cs
+++ Scripts/Editor/ModelPostprocessor.cs
@@ -32,12 +32,13 @@ namespace RyanQuagliataUnity.Editor {
 
 			// Only check imported assets
 			foreach (var path in importedAssets) {
+#if ODIN_INSPECTOR
+				if (!ModelPostProcessorConfig.Instance.CreatePrefabVariantsForModels) continue;
+#endif
+				
 				// Load the asset from the path
 				var prefab = AssetDatabase.LoadMainAssetAtPath(path);
-				if (prefab == null) {
-					Debug.LogError($"Failed to loading asset at path {path}");
-					continue;
-				}
+				if (prefab == null) continue;
 
 				// Make sure this is a prefab
 				if (PrefabUtility.GetPrefabInstanceStatus(prefab) != PrefabInstanceStatus.Connected) return;
@@ -52,10 +53,6 @@ namespace RyanQuagliataUnity.Editor {
 				// Skip SVG files
 				if (AssetImporter.GetAtPath(path) is SVGImporter) return;
 #endif
-#if ODIN_INSPECTOR
-				if (!ModelPostProcessorConfig.Instance.CreatePrefabVariantsForModels) continue;
-#endif
-
 				var prefabPath = Path.ChangeExtension(path, ".prefab");
 				if (File.Exists(Path.GetFullPath(prefabPath))) continue;
 
