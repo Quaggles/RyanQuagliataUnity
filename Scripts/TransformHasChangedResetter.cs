ebdaf428120ca6c71ee22f16474b84008039a060
Ryan Quagliata <r.quagliata@qut.edu.au>
2021-03-19 14:02:00 +1000

    TransformHasChangedResetter hidden MonoScript field

--- Scripts/TransformHasChangedResetter.cs
+++ Scripts/TransformHasChangedResetter.cs
@@ -3,31 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace RyanQuagliataUnity {
-    [DisallowMultipleComponent]
-    public class TransformHasChangedResetter : MonoBehaviour {
-        public static YieldInstruction WaitYield = null;
-        private static bool routineStarted = false;
-        public static List<TransformHasChangedResetter> All = new List<TransformHasChangedResetter>();
+	[DisallowMultipleComponent]
+#if ODIN_INSPECTOR
+	[Sirenix.OdinInspector.HideMonoScript]
+#endif
+	public class TransformHasChangedResetter : MonoBehaviour {
+		public static YieldInstruction WaitYield = null;
+		private static bool routineStarted = false;
+		public static List<TransformHasChangedResetter> All = new List<TransformHasChangedResetter>();
 
-        private void OnEnable() => All.Add(this);
-        private void OnDisable() => All.Remove(this);
+		private void OnEnable() => All.Add(this);
+		private void OnDisable() => All.Remove(this);
 
-        void Start() {
-            if (!routineStarted) {
-                CoroutineHost.Run(EndOfFrameReset());
-                routineStarted = true;
-            }
-        }
+		void Start() {
+			if (!routineStarted) {
+				CoroutineHost.Run(EndOfFrameReset());
+				routineStarted = true;
+			}
+		}
 
-        /// <summary>
-        /// This coroutine will always be looping over every object and clearing the has
-        /// </summary>
-        /// <returns></returns>
-        static IEnumerator EndOfFrameReset() {
-            while (Application.isPlaying) {
-                yield return WaitYield;
-                foreach (var transformHasChangedResetter in All) transformHasChangedResetter.transform.hasChanged = false;
-            }
-        }
-    }
+		/// <summary>
+		/// This coroutine will always be looping over every object and clearing the has
+		/// </summary>
+		/// <returns></returns>
+		static IEnumerator EndOfFrameReset() {
+			while (Application.isPlaying) {
+				yield return WaitYield;
+				foreach (var transformHasChangedResetter in All) transformHasChangedResetter.transform.hasChanged = false;
+			}
+		}
+	}
 }
